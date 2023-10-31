0eea46b8bc73dc576bdac4562c3bcfc2b3c56ae5
Martin Kristof <martin.kristof@heureka.cz>
2023-10-31 22:39:25 +0100

    chore: step-8 user post detail page finished

--- src/app/(user)/posts/[id]/page.tsx
+++ src/app/(user)/posts/[id]/page.tsx
@@ -1,3 +1,7 @@
+import { Container } from '@techmeetup/app/_components/ui/Container';
+import { getPostById, getPosts } from '@techmeetup/libs/postsQuery';
+import { Metadata } from 'next';
+import { notFound } from 'next/navigation';
 import React, { FC } from 'react';
 
 interface PostDetailPageProps {
@@ -5,9 +9,35 @@ interface PostDetailPageProps {
 }
 
 // SSG
+export const generateMetadata = async ({ params: { id } }: PostDetailPageProps): Promise<Metadata> => {
+  const { post } = await getPostById(id);
+
+  return {
+    title: `Post! TechMeetup - Post ${post?.title || id} detail`,
+  };
+};
+
+export const generateStaticParams = async () => {
+  const { posts } = await getPosts();
+
+  return posts?.map(({ _id: id }) => ({
+    id: id.toString(),
+  }));
+};
 
 const PostDetailPage: FC<PostDetailPageProps> = async ({ params: { id } }) => {
-  return <p>Post detail page</p>;
+  const { post } = await getPostById(id);
+
+  return post ? (
+    <Container>
+      <h1 className="text-2xl">{post.title}</h1>
+      <div className="mt-2">
+        <p>{post.description}</p>
+      </div>
+    </Container>
+  ) : (
+    notFound()
+  );
 };
 
 export default PostDetailPage;
