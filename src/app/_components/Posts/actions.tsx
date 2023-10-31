8a77e43bde2dbfe4e7dc44d65180d377254bb8d9
Martin Kristof <martin.kristof@heureka.cz>
2023-10-31 22:19:15 +0100

    chore: clean delete post

--- src/app/_components/Posts/actions.tsx
+++ src/app/_components/Posts/actions.tsx
@@ -1,21 +1,9 @@
 // 'use server';
 
-import { revalidatePath } from 'next/cache';
-import { isInFavorites } from '@techmeetup/libs/favorites';
-import { deletePostById } from '@techmeetup/libs/postsQuery';
-
 export const toggleFavorite = async (id: string) => {
   return undefined;
 };
 
 export const deletePost = async (id: string) => {
-  await deletePostById(id);
-
-  revalidatePath('/posts', 'page');
-  revalidatePath('/admin', 'page');
-  revalidatePath('/(user)/posts/[id]', 'page');
-
-  if (isInFavorites(id)) {
-    toggleFavorite(id);
-  }
+  return undefined;
 };
